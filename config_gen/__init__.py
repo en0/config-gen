48ca71b2b96b1138e992f7c6180f21ac725e7013
Ian Laird <irlaird@gmail.com>
2015-12-30 19:27:46 -0700

    Exposed keystore interface

--- config_gen/__init__.py
+++ config_gen/__init__.py
@@ -1,10 +1,12 @@
 from library import Library
 from keystore import Keystore
 from template_processor import TemplateProcessor
+from keystore_interface import KeystoreInterface
 
 
 __all__ = [
     "Library",
     "Keystore",
-    "TemplateProcessor"
+    "TemplateProcessor",
+    "KeystoreInterface"
 ]
