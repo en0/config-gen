1f43adfb0fb2f1fe0c54c74e93d1429a84ebdbd3
Ian Laird <irlaird@gmail.com>
2016-01-10 22:23:56 -0700

    Rolled version and added redis to required install list

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name="config-gen",
-    version="1.0",
+    version="1.1",
     packages=['config_gen', 'config_gen.adapters'],
     description="Configuration Generation using templates.",
     author="Ian Laird",
     author_email="en0@mail.com",
     url="https://github.com/en0/config-gen/",
-    install_requires=['jinja2'],
+    install_requires=['jinja2', 'redis'],
     entry_points={
         'console_scripts': [
             'config-gen-init = config_gen.__main__:main_init',
